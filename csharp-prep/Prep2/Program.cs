06e30da14caf65c99c8582dc21da1e3373662fd1
Jacob Bree <bre21020@gmail.com>
2026-01-10 11:56:46 -0700

    Homework

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Put code here
+        Console.Write("What is your grade percentage? (ex: 85) ");
+        int grade = int.Parse(Console.ReadLine());
+        //Console.WriteLine(grade);
+        string letter = "default";
+        string message = " you pass!";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        } else if (grade >= 80)
+        {
+            letter = "B";
+        } else if (grade >= 70)
+        {
+            letter = "C";
+        } else if (grade >= 60)
+        {
+            letter = "D";
+        } else
+        {
+            letter = "F";
+            message = " YOU FAIL!";
+        }
+        Console.WriteLine(letter + message);
     }
 }
