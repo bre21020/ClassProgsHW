44669a839d7da67a94fda0f5cdfd1430c3bfb29d
Jacob Bree <bre21020@gmail.com>
2026-01-10 10:02:00 -0700

    test

---
+++ csharptestfile.cs
@@ -0,0 +1,34 @@
+int x;
+x = 5;
+Console.WriteLine(x);
+
+int y = 5;
+Console.WriteLine(y);
+
+string test = "Hello, World!";
+Console.WriteLine(test);
+
+float z = 3.14f;
+Console.WriteLine(z);
+
+double pi = 3.14159;
+Console.WriteLine(pi);
+
+bool isTrue = true;
+Console.WriteLine(isTrue);
+
+Console.WriteLine("There will not be a newline after this.");
+Console.WriteLine("Well?");
+
+Console.Write("What is your favorite color? ");
+string color = Console.ReadLine();
+Console.WriteLine(color);
+
+if (x > y)
+{
+    Console.WriteLine("Greater");
+}
+else
+{
+    Console.WriteLine("Less than or equal to");
+}
