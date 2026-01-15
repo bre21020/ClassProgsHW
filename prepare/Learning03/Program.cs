04a5e57991155b4dcd48efe1ce79b3d4f2a569c1
Jacob Bree <bre21020@gmail.com>
2026-01-15 09:33:16 -0700

    lists

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Console.WriteLine("Hello Learning03 World!");
+
+        //string response;
+        
+        //while (response == "yes") {
+        //    Console.Write("Do you want to continue? ");
+        //    response = Console.ReadLine();
+        //}
+
+        //do {
+        //    Console.Write("Do you want to continue? ");
+        //    response = Console.ReadLine();       
+        //} while (response == "yes");
+
+        //for (int i = 0; i < 10; i++) {
+        //    Console.WriteLine(i);
+        //}        
+
+        //for (int i = 2; i <= 20; i = i + 2) {
+        //    Console.WriteLine(i);
+        //}
+
+        //Console.WriteLine("bye");
+
+        //foreach (string color in colors) {
+        //    Console.WriteLine(color);
+        //}
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
+
+        int guess = 0;
+        int guesses = 0;
+
+        while (guess != number) {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            //Console.WriteLine(number);
+            guesses ++;
+            if (guess > number) {
+                Console.WriteLine("The number is less than " + guess);
+            } else if (guess < number) {
+                Console.WriteLine("The number is greater than " + guess);
+            } else {
+                //Console.WriteLine("error");
+            }
+        }
+        if (guesses == 1) {
+        Console.WriteLine("You got it in 1 guess!");
+        } else { 
+            Console.WriteLine("You got it in " + guesses + " guesses!");
+        }
     }
 }
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Console.WriteLine("Hello Learning04 World!");
+        //List<int> numbers;
+        //List<string> words;
+        //numbers = new List<int>();
+
+        //List<int> numbers2 = new List<int>();
+        //List<string> words2 = new List<string>();
+
+        //using System.Collections.Generic;
+
+        ////////////////////////////
+
+        //List<string> words = new List<string>();
+
+        //words.Add("phone");
+        //words.Add("keyboard");
+        //words.Add("mouse");
+        //words.Add("desk");
+
+        //Console.WriteLine(words.Count);
+
+        //foreach (string word in words) {
+        //    Console.WriteLine(word);
+        //}
+
+        //for (int i = 0; i < words.Count; i ++) {
+        //    Console.WriteLine(words[i]);
+        //}
+
+        List<int> numbers = new List<int>();
+        int number = 999;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int sum = 0;
+        
+
+        while (number != 0) {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number == 0) {
+                break;
+            } else {
+                numbers.Add(number);
+                sum += number;
+            }
+
+            
+        }
+
+        int max = numbers[0];
+
+        foreach (int number2 in numbers) {
+            Console.WriteLine(number2);
+            if (number2 > max) {
+                max = number2;
+            }
+        }
+        float average = sum / numbers.Count();
+        Console.WriteLine("Sum of all numbers in the list is " + sum);
+        Console.WriteLine("Max number in the list is " + max);
+        Console.WriteLine("The average for this list is " + average);
     }
 }
